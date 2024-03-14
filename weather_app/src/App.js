b1ad06a6a3c3a0e9c8516b05dc5fb219e9950ab0
Pritam Maharjan <primgdev@gmail.com>
2024-03-14 22:14:19 +1100

    update

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return
-  (
-    <div className="searchBar">
-     
-
-
-    </div>
-
-  )
   
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+  
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+  
+      console.log("Latitude is:", lat)
+      console.log("Longitude is:", long)
+    }, [lat, long]);
+
+    return (
+      <div className="App">
+  
+      </div>
+    );
+    
 }
 
 export default App;
